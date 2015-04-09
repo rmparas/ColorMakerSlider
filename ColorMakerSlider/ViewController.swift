cbce00f85581f57991452db3d113f0f10cd405e2
rmparas <rmparas@yahoo.com>
2015-04-08 21:43:02 -0700

    initial load

--- ColorMakerSlider/ViewController.swift
+++ ColorMakerSlider/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+   
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeColorComponent(sender: AnyObject) {
+        
+        let rcgf: CGFloat = CGFloat(self.redSlider.value)
+        let gcgf: CGFloat = CGFloat(self.greenSlider.value)
+        let bcgf: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: rcgf, green: gcgf, blue: bcgf, alpha: 1)
     }
-
+    
 
 }
 
